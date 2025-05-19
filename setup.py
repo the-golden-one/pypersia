75bc6cc541b2c037efa4bc1071bff4d142e9e59e
Aria <aria.farahmand1391@gmail.com>
2025-05-19 16:09:25 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pypersia",
-    version="1.3.0",
+    version="1.4.0",
     description="A Python library for Persian utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",

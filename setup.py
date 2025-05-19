a30b95c481614848d87c2bfe938399c798acabd2
Aria <aria.farahmand1391@gmail.com>
2025-05-19 15:50:08 +0330

    Update setup.py
    Added PyPi required desc

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='pypersia',
-    version='0.1',
-    license='MIT',
-    description='A Persian utility library for digit conversion, calendar calculations, text normalization, and more.',
-    author='Aria Farahmand',
-    packages=['pypersia'],
+    name="pypersia",
+    version="1.0.0",
+    description="A Python library for Persian utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["pypersia"],
 )
